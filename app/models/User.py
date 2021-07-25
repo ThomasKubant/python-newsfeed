25710aa7105fba289c4691cdc542a343a3180059
reanna swick <thomaskubant@gmail.com>
2021-07-24 21:01:21 -0700

    setup server using Flask, started SQL database and user model using SQLAlchemy

---
+++ app/models/User.py
@@ -0,0 +1,19 @@
+from app.db import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+
+class User(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True)
+  username = Column(String(50), nullable=False)
+  email = Column(String(50), nullable=False, unique=True)
+  password = Column(String(100), nullable=False)
+  @validates('email')
+  def validate_email(self, key, email):
+    # make sure email address contains @ character
+    assert '@' in email
+    return email
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+    return password
