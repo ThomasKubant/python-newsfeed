0dfc39d44ffbfede5f7a0e7ad04ae1d4fa9948ee
ThomasKubant <thomaskubant@gmail.com>
2021-07-26 13:22:07 -0700

    finished post and user model

--- app/models/User.py
+++ app/models/User.py
@@ -1,6 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import validates
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -16,4 +19,4 @@ class User(Base):
   @validates('password')
   def validate_password(self, key, password):
     assert len(password) > 4
-    return password
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
