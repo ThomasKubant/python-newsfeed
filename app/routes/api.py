d8dbc6019819f532beebd7d41fc8c83042be8448
reanna swick <thomaskubant@gmail.com>
2021-08-18 20:09:44 -0700

    finished app, needs some optimization before deployment

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -52,3 +53,98 @@ def login():
     except: print(sys.exc_info()[0])
 
     return jsonify(message = 'Incorrect credentials'), 400
+
+@bp.route('/comments', methods=['POST'])
+@login_required
+def comment():
+    data = request.get_json()
+    db = get_db()
+    
+    try:
+        newComment = Comment(
+            comment_text = data['comment_text'],
+            post_id = data['post_id'],
+            user_id = session.get('user_id')
+        )
+        db.add(newComment)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Comment failed'), 500
+    return jsonify(id = newComment.id)
+
+@bp.route('/posts/upvote', methods=['PUT'])
+@login_required
+def upvote():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        newVote = Vote(
+            post_id = data['post_id'],
+            user_id = session.get('user_id')
+        )
+
+        db.add(newVote)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Upvote failed'), 500
+    
+    return '', 204
+
+@bp.route('/posts', methods=['POST'])
+@login_required
+def create():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        newPost = Post(
+            title = data['title'],
+            post_url = data['post_url'],
+            user_id = session.get('user_id')
+        )
+
+        db.add(newPost)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Post failed'), 500
+
+    return jsonify(id = newPost.id)
+
+@bp.route('/posts/<id>', methods=['PUT'])
+@login_required
+def update(id):
+    data = request.get_json()
+    db = get_db()
+    try:
+        post = db.query(Post).filter(Post.id == id).one()
+        post.title = data['title']
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message = 'Post not found'), 404
+    return '', 204
+
+@bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
+def delete(id):
+    db = get_db()
+
+    try:
+        db.delete(db.query(Post).filter(Post.id == id).one())
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message = 'Post not found'), 404
+    return '', 204
